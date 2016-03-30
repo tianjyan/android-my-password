47bd35b5e7aca5eb63502742c338def30175909c
youngytj <youngytj@sina.com>
2016-03-30 08:07:44 +0800

    New Init

---
+++ MyPassword/app/src/main/java/young/home/com/mypassword/activity/StartActivity.java
@@ -0,0 +1,15 @@
+package young.home.com.mypassword.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import young.home.com.mypassword.R;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+}
