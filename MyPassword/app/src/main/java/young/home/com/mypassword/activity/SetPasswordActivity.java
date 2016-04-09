aaaa9a89f11380f70ff4f7c4756de5d449432540
youngytj <youngytj@sina.com>
2016-04-09 09:09:00 +0800

    Change SetPassword and Start

--- MyPassword/app/src/main/java/young/home/com/mypassword/activity/SetPasswordActivity.java
+++ MyPassword/app/src/main/java/young/home/com/mypassword/activity/SetPasswordActivity.java
@@ -1,22 +1,25 @@
 package young.home.com.mypassword.activity;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
-import java.security.NoSuchAlgorithmException;
 
 import young.home.com.mypassword.R;
 import young.home.com.mypassword.application.BaseActivity;
 import young.home.com.mypassword.application.MD5;
 import young.home.com.mypassword.model.SettingKey;
 
-public class SetPasswordActivity extends BaseActivity {
+public class SetPasswordActivity extends BaseActivity implements TextWatcher {
 
     private EditText pwdEt;
     private EditText rePwdEt;
+    private Button nextBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +28,22 @@ public class SetPasswordActivity extends BaseActivity {
 
         pwdEt = (EditText)findViewById(R.id.PwdEt);
         rePwdEt = (EditText)findViewById(R.id.RePwdEt);
+        nextBtn = (Button)findViewById(R.id.nextBtn);
+
+        pwdEt.addTextChangedListener(this);
+        rePwdEt.addTextChangedListener(this);
+
+        showFirstUserDialog();
     }
 
-    public void EnterClick(View v) {
+    private void showFirstUserDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(R.string.set_lock_pattern_first_message);
+        builder.setPositiveButton(R.string.set_lock_pattern_first_sure, null);
+        builder.show();
+    }
+
+    public void nextClick(View v) {
         if(pwdEt.getText().toString().equals(rePwdEt.getText().toString())){
             String pwd = pwdEt.getText().toString();
             super.putSetting(SettingKey.LOCK_PWD, MD5.getMD5(pwd));
@@ -37,8 +53,27 @@ public class SetPasswordActivity extends BaseActivity {
             finish();
         }
         else{
-            Toast.makeText(this, "两次密码不同", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, R.string.different_pwd, Toast.LENGTH_SHORT).show();
         }
     }
 
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+        if(pwdEt.getText().length() > 0 && rePwdEt.getText().length() > 0){
+            nextBtn.setEnabled(true);
+        }
+        else{
+            nextBtn.setEnabled(false);
+        }
+    }
 }
--- MyPassword/app/src/main/java/young/home/com/mypassword/activity/StartActivity.java
+++ MyPassword/app/src/main/java/young/home/com/mypassword/activity/StartActivity.java
@@ -1,10 +1,20 @@
 package young.home.com.mypassword.activity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import org.w3c.dom.Text;
 
 import java.security.NoSuchAlgorithmException;
 
@@ -13,9 +23,11 @@ import young.home.com.mypassword.application.BaseActivity;
 import young.home.com.mypassword.application.MD5;
 import young.home.com.mypassword.model.SettingKey;
 
-public class StartActivity extends BaseActivity {
+public class StartActivity extends BaseActivity implements TextWatcher{
 
     EditText inputPwd;
+    Button enterBtn;
+
     String pwd;
 
     @Override
@@ -23,24 +35,45 @@ public class StartActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
-       pwd =  super.getSetting(SettingKey.LOCK_PWD, "");
-        if(pwd.isEmpty()){
+        pwd = super.getSetting(SettingKey.LOCK_PWD, "");
+        if (pwd.isEmpty()) {
             Intent intent = new Intent(this, SetPasswordActivity.class);
             startActivity(intent);
             finish();
         }
 
-        inputPwd = (EditText)findViewById(R.id.InputPwd);
+        inputPwd = (EditText) findViewById(R.id.inputPwd);
+        enterBtn = (Button)findViewById(R.id.enterBtn);
+
+        inputPwd.addTextChangedListener(this);
     }
 
-    public void EnterClick(View v) {
-        if(pwd.equals(MD5.getMD5(inputPwd.getText().toString()))) {
+    public void enterClick(View v) {
+        if (pwd.equals(MD5.getMD5(inputPwd.getText().toString()))) {
             Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
             finish();
+        } else {
+            Toast.makeText(this, R.string.wrong_password, Toast.LENGTH_SHORT).show();
         }
-        else {
-            Toast.makeText(this, "密码不正确", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+        if(inputPwd.getText().length() > 0) {
+            enterBtn.setEnabled(true);
+        } else {
+            enterBtn.setEnabled(false);
         }
     }
 }
