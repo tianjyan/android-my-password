6dd83e88c158770343e1a83a97d72f4be848246e
youngytj <YOUNG@YOUNG-MBP.local>
2016-05-23 07:58:01 +0800

    Change Package Name

--- MyPassword/app/src/main/java/young/home/com/mypassword/service/OnPasswordGroupSelected.java
+++
@@ -1,8 +0,0 @@
-package young.home.com.mypassword.service;
-
-/**
- * Created by YOUNG on 2016/4/9.
- */
-public interface OnPasswordGroupSelected {
-    void onPasswordGroupSelected(String passwordGroupName);
-}
